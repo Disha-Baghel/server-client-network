74829e178d32076b500ec8f1f758d5e144ac438b
disha-baghel <bagheldisha708@gmail.com>
2024-03-23 21:52:38 +0530

    server

--- client.cpp
+++ client.cpp
@@ -2,10 +2,29 @@
 #include <iostream> 
 #include <netinet/in.h> 
 #include <sys/socket.h> 
-#include <unistd.h> 
+#include <unistd.h>
+#include <thread>
 
-int main() 
-{ 
+void sendMessage (int clientSocket) {
+	while(true) {
+	char* message = new char[1000];
+	std::cout<<"Client: ";
+	std::cin.getline(message, 1000);
+	send(clientSocket, message, strlen(message), 0); 
+	}
+
+}
+
+void receiveMessage (int clientSocket) {
+	while(true) {
+		char buffer[1024] = {0};
+		recv(clientSocket, buffer, sizeof(buffer), 0);
+		std::cout<< "Server: "<< buffer <<std::endl;
+	}
+
+}
+
+int main() { 
 	int clientSocket = socket(AF_INET, SOCK_STREAM, 0); 
 
 	sockaddr_in serverAddress; 
@@ -13,17 +32,17 @@ int main()
 	serverAddress.sin_port = htons(8080); 
 	serverAddress.sin_addr.s_addr = INADDR_ANY; 
 
-	connect(clientSocket, (struct sockaddr*)&serverAddress, 
-			sizeof(serverAddress)); 
+	connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)); 
+	
+	std::thread send (sendMessage, clientSocket);
+	std::thread receive (receiveMessage, clientSocket);
 
-	const char* message = "Hi"; 
-	send(clientSocket, message, strlen(message), 0); 
+		
+	send.join();
+	receive.join();
 
-	char buffer[1024] = {0};
-	recv(clientSocket, buffer, sizeof(buffer), 0);
-	std::cout<< "Server: "<< buffer <<std::endl;
 
-	close(clientSocket); 
+	close(clientSocket);
 
-	return 0; 
+	return 0;
 }
