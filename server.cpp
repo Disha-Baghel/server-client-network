a1cd98851e2313ad6e4fb165646480b4ba7712e5
disha-baghel <bagheldisha708@gmail.com>
2024-03-23 21:53:00 +0530

    client

--- server.cpp
+++ server.cpp
@@ -1,38 +1,53 @@
-#include <cstring> 
-#include <iostream> 
-#include <netinet/in.h> 
-#include <sys/socket.h> 
-#include <unistd.h> 
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <thread>
 
-using namespace std; 
+using namespace std;
 
-int main() 
-{ 
-	int serverSocket = socket(AF_INET, SOCK_STREAM, 0); 
+void sendMessage (int clientSocket) {
 
-	sockaddr_in serverAddress; 
-	serverAddress.sin_family = AF_INET; 
-	serverAddress.sin_port = htons(8080); 
-	serverAddress.sin_addr.s_addr = INADDR_ANY; 
+	while(true) {
+		char *message = new char[1000];
+		cout << "Server: ";
+		cin.getline(message, 1000);
 
-	bind(serverSocket, (struct sockaddr*)&serverAddress, 
-		sizeof(serverAddress)); 
+		send(clientSocket, message, strlen(message), 0);
+	}
 
-	listen(serverSocket, 5); 
+}
 
-	int clientSocket 
-		= accept(serverSocket, nullptr, nullptr); 
+void receiveMessage (int clientSocket) {
+	while (true) {
+		char buffer[1024] = {0};
+		recv(clientSocket, buffer, sizeof(buffer), 0);
+		cout << "Message from client: " << buffer << endl;
+	}
+} 
 
-	char buffer[1024] = { 0 }; 
-	recv(clientSocket, buffer, sizeof(buffer), 0); 
-	cout << "Message from client: " << buffer 
-			<< endl; 
+int main() {
+	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-	const char* message = "hello";
-	send(clientSocket, message, strlen(message), 0);
+	sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(8080);
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-	close(clientSocket);
-	close(serverSocket); 
+	bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
 
-	return 0; 
+	listen(serverSocket, 5);
+
+	int clientSocket = accept(serverSocket, nullptr, nullptr);
+
+	std::thread send(sendMessage, clientSocket);
+	std::thread receive(receiveMessage, clientSocket);
+
+	send.join();
+	receive.join();
+
+	close(serverSocket);
+
+	return 0;
 }
